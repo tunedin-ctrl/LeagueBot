a5340c2d2a60a38089c4b5bc9a659e7beed7b4f0
tunein <z5371683@ad.unsw.edu.au>
2022-05-20 20:14:18 +1000

    Implementing match hsit

--- src/cogs/lol.py
+++ src/cogs/lol.py
@@ -25,12 +25,17 @@ class Lol(discord.ext.commands.Cog, name='Lol module'):
         response = requests.get(f"https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}")
         # print(response.)
         if response.status_code == 200:
-            message = json.loads(response.text)
-            print(message)
-            print(message["id"])
+            summoner_info = json.loads(response.text)
+            puuid = summoner_info["puuid"]
+            match_ids = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=5&api_key=RGAPI-077d0358-b6a2-4501-8739-700d1a9da7dd')
+            matchId = json.loads(match_ids.text)
+            for match_id in matchId:
+                match = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key=RGAPI-077d0358-b6a2-4501-8739-700d1a9da7dd')
+                print(match.text)
+
         else:
             message = api_error.AccessError(description=f"Summoner name {name} you have provided does not exist")                
             raise api_error.AccessError(description=f"Summoner name {name} you have provided does not exist")
 
             
-        await ctx.send(message)
+        await ctx.send(match_ids)
