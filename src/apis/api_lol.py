21f39a7d5188868532e8dff7814eeaef2adaea75
tunein <z5371683@ad.unsw.edu.au>
2022-05-22 22:08:31 +1000

    Finished cleansing the data and produced in csv

--- src/apis/api_lol.py
+++ src/apis/api_lol.py
@@ -1,38 +1,65 @@
 import json
 import os
 import requests
+import pandas
 from dotenv import load_dotenv
 from src.error_handler import api_error
+
+load_dotenv()
+api_key = os.environ.get('API_KEY')
+
 
 def lolAnalysis(name):
     """
     Input: summoner Id
     Output: Match history
     """
-    load_dotenv()
-    api_key = os.environ.get('API_KEY')
+    summoner_info = get_summoner_info(name)
+    puuid = summoner_info["puuid"]
+    match_ids = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?type=ranked&start=0&count=2&api_key={api_key}')
+    if match_ids.status_code == 200:
+        matchId = json.loads(match_ids.text)
+        json_array = {
+            f'{name}': []
+        }
+        for match_id in matchId:
+            match = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={api_key}')
+            if match.status_code == 200:
+                match_det = json.loads(match.text)
+                for sumName in match_det['info']['participants']:
+                    del sumName['perks']
+                    if sumName['summonerName'] == name:
+                        json_array[name].append(sumName)
+        with open('lol_db.json', 'a') as match_file:
+            json.dump(json_array, match_file, indent=4, separators=(',',': '))
+            match_file.write('\n')
+        
+        with open('lol_db.json', encoding='utf-8') as inputfile:
+            df = pandas.read_json(inputfile)
+        df.to_csv('csvfile.csv', encoding='utf-8', index=False)
 
-    response = requests.get(f"https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}")
-    if response.status_code == 200:
-        summoner_info = json.loads(response.text)
-        puuid = summoner_info["puuid"]
-        match_ids = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?type=ranked&start=0&count=2&api_key={api_key}')
-        if match_ids.status_code == 200:
-            matchId = json.loads(match_ids.text)
-            # match_files = dict()
-            for match_id in matchId:
-                match = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={api_key}')
-                if match.status_code == 200:
-                    match_det = json.loads(match.text)
-                    for sumName in match_det['info']['participants']:
-                        if sumName['summonerName'] == name:
-                            with open('lol_db.json', 'w') as match_file:
-                                json.dumps(sumName, match_file,
-                                         indent = 4)
     else:
-        return api_error.AccessError(description=f"API key has expired!")                
+        return api_error.AccessError(description=f"Something went wrong! Please try again") 
+
+def get_summoner_info(name):
+    '''
+    input: name:str, api_key:riot key
+    output: summoner_info: dict
+    '''
+    try:
+        response = requests.get(f"https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={api_key}")
+        if response.status_code == 200:
+            summoner_info = json.loads(response.text)
+            return summoner_info
+    except:
+        raise api_error.AccessError(description=f"summoner id not found!")      
 
 
 def helper_decision_tree():
-        pass
+    ''' 
+    once all operations are done, then delete data
+    later stored in mongodb if enough space
+    '''
+
+    pass
     
--- tests/test_api_lol.py
+++ tests/test_api_lol.py
@@ -1,10 +1,16 @@
-import json
 from src.apis import api_lol
 
 def test_user_details():
     '''
     user does not exist when inserting
     '''
-    name="MiliterryNasus"
+    name= "MiliterryNasus"
+    name2= "tunein"
+    output = (api_lol.get_summoner_info(name))
+    output2 = (api_lol.get_summoner_info(name2))
+    assert output['name'] == name
+    assert output2['name'] == name2
+    
+def test_decision_tree():
+    name= "MiliterryNasus"
     api_lol.lolAnalysis(name)
-        
